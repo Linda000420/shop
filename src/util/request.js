84ca57fc5a6a84a9483617b571a15511a680a97d
Lin <1030863031@qq.com>
2024-05-04 20:43:09 +0800

    完成拦截器错误处理

--- src/util/request.js
+++ src/util/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Toast } from 'vant'
 // 创建 axios 实例，将来对创建出来的实例进行自定义配置
 // 不会污染原始的 axios 实例
 const instance = axios.create({
@@ -19,7 +20,14 @@ instance.interceptors.request.use(function (config) {
 // 添加响应拦截器
 instance.interceptors.response.use(function (response) {
   // 对响应数据做点什么（默认 axios 会多包装一层 data，需要响应拦截器中处理一下）
-  return response.data
+  const res = response.data
+  if (res.status !== 200) {
+    //  给提示
+    Toast(res.message)
+    //  抛出一个错误的 promise
+    return Promise.reject(res.message)
+  }
+  return res
 }, function (error) {
   // 对响应错误做点什么
   return Promise.reject(error)
