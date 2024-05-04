103a697289367fb91fc5afd6eea965cd3f5fb36a
Lin <1030863031@qq.com>
2024-05-04 22:53:57 +0800

    完成首页静态布局

--- src/util/vant-ui.js
+++ src/util/vant-ui.js
@@ -1,8 +1,14 @@
 //  按需导入
 import Vue from 'vue'
-import { Tabbar, TabbarItem, NavBar, Toast } from 'vant'
+import { Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, Button, SwipeItem, Grid, GridItem } from 'vant'
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(NavBar)
 Vue.use(Toast)
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(Button)
+Vue.use(SwipeItem)
+Vue.use(Grid)
+Vue.use(GridItem)
